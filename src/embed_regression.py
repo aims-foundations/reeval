f5b84733d935cace16d9bd9bf4be0250d00c1536
yuhengtu <yuhengtuece@gmail.com>
2024-08-31 19:09:23 -0700

    Update embed_regression.py

--- src/embed_regression.py
+++ src/embed_regression.py
@@ -1,11 +1,16 @@
 from embed_text_package.embed_text import Embedder
+from torch.utils.data import DataLoader
 from datasets import load_dataset
 
 if __name__ == "__main__":
-    dataset = load_dataset("stair-lab/airbench-difficulty")
-    print(dataset)
-
-    # model_name = "<HF_repo>/<HF_model>"
-    # embdr = Embedder()
-    # embdr.load(model_name)
-    # emb = embdr.get_embeddings(dataloader, MODEL_NAME, cols_to_be_embded)
+    dataset = load_dataset("stair-lab/airbench-difficulty", split="whole")
+    cols_to_be_embded = ['question_text']
+    bs = 1024
+    model_name = "meta-llama/Meta-Llama-3-8B"
+    
+    embdr = Embedder()
+    embdr.load(model_name)
+    dataloader = DataLoader(dataset.with_format("torch"), bs)
+    emb = embdr.get_embeddings(
+        dataloader, model_name, cols_to_be_embded
+    )
