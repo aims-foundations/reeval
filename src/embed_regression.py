23250151dfb2d6669b61aba3385257557f69195c
yuhengtu <yuhengtuece@gmail.com>
2024-08-31 18:59:48 -0700

    Update embed_regression.py

--- src/embed_regression.py
+++ src/embed_regression.py
@@ -1,6 +1,11 @@
 from embed_text_package.embed_text import Embedder
+from datasets import load_dataset
 
-model_name = "<HF_repo>/<HF_model>"
-embdr = Embedder()
-embdr.load(model_name)
-emb = embdr.get_embeddings(dataloader, MODEL_NAME, cols_to_be_embded)
+if __name__ == "__main__":
+    dataset = load_dataset("stair-lab/airbench-difficulty")
+    print(dataset)
+
+    # model_name = "<HF_repo>/<HF_model>"
+    # embdr = Embedder()
+    # embdr.load(model_name)
+    # emb = embdr.get_embeddings(dataloader, MODEL_NAME, cols_to_be_embded)
