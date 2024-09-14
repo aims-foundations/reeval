62e6ba8639a5b4c5cff30027b7bf02f1849c7669
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-09-13 23:07:40 -0700

    new

--- src/crawl/res_mat_synthetic_reasoning.py
+++ src/crawl/res_mat_synthetic_reasoning.py
@@ -13,9 +13,10 @@ if __name__ == "__main__":
         "synthetic_reasoning_natural:difficulty=hard"
     ]
     
+    # non mask matrix
     all_responses_df = pd.DataFrame()
     min_lens = []
-    
+
     for start_string in start_strings:
         min_length = float('inf')
         for json_file in os.listdir(json_dir):
@@ -53,10 +54,9 @@ if __name__ == "__main__":
         else:
             assert (all_responses_df.index == response_df.index).all(), "Model names do not match!"
             all_responses_df = pd.concat([all_responses_df, response_df], axis=1)
+            
     all_responses_df.columns = [f'{i}' for i in range(all_responses_df.shape[1])]
-    all_responses_df.to_csv(f'{output_dir}/all_matrix.csv', index_label=None)
     
-    # index search file
     bool_delete_list = []
     for col_name, col_data in all_responses_df.items():
         if col_data.nunique() == 1:
@@ -65,8 +65,8 @@ if __name__ == "__main__":
         else:
             bool_delete_list.append(0)
 
-    output_file = f"{output_dir}/index_search.csv"
-
+    # index search file
+    output_file = f"{output_dir}/non_mask_index_search.csv"
     output_data = []
     base_idx = 0
     for i, start_string in enumerate(start_strings):
@@ -83,4 +83,88 @@ if __name__ == "__main__":
 
     output_df = pd.DataFrame(output_data, columns=["idx", "text", "is_deleted"])
     output_df.to_csv(output_file, index=False)
+
+    all_responses_df.columns = [f'{i}' for i in range(all_responses_df.shape[1])]
+    all_responses_df.to_csv(f'{output_dir}/non_mask_matrix.csv', index_label=None)
+
+
+
+    # mask matrix
+    all_responses_df = pd.DataFrame()
+    max_lens = []
+    for start_string in start_strings:
+        max_length = 0
+        for json_file in os.listdir(json_dir):
+            if json_file.endswith('.json') and json_file.startswith(start_string):
+                with open(f"{json_dir}/{json_file}", 'r') as f:
+                    data = json.load(f)
+                len_q = len(data['request_states'])
+                if len_q > max_length:
+                    max_length = len_q
+        print(f"dataset {start_string}, max length {max_length}")
+        max_lens.append(max_length)
+        
+        response_matrix = {}
+        for json_file in os.listdir(json_dir):
+            if json_file.endswith('.json') and json_file.startswith(start_string):
+                with open(f"{json_dir}/{json_file}", 'r') as f:
+                    data = json.load(f)
+                
+                model_name = data['adapter_spec']['model']
+                correct_answers = []
+                
+                for idx, question in enumerate(data['request_states']):
+                    predicted_answer = question['result']['completions'][0]['text'].strip()
+                    true_answer = question['instance']['references'][0]['output']['text'].strip()
+                    correct_answers.append(int(predicted_answer==true_answer))
+
+                if len(correct_answers) < max_length:
+                    correct_answers.extend([-1] * (max_length - len(correct_answers)))
+                response_matrix[model_name] = correct_answers
+           
+        response_df = pd.DataFrame(response_matrix).T
+        response_df = response_df.sort_index(axis=0)
+        
+        if all_responses_df.empty:
+            all_responses_df = response_df
+        else:
+            assert (all_responses_df.index == response_df.index).all(), "Model names do not match!"
+            all_responses_df = pd.concat([all_responses_df, response_df], axis=1)
+            
+    all_responses_df.columns = [f'{i}' for i in range(all_responses_df.shape[1])]
+    
+    bool_delete_list = []
+    for col_name, col_data in all_responses_df.items():
+        if set(col_data.unique()).issubset({0, -1}):
+            all_responses_df = all_responses_df.drop(columns=[col_name])
+            bool_delete_list.append(1)
+        else:
+            bool_delete_list.append(0)
+
+    # index search file
+    output_file = f"{output_dir}/mask_index_search.csv"
+    output_data = []
+    base_idx = 0
+    for i, start_string in enumerate(start_strings):
+        input_file = f"{json_dir}/{start_string},model=AlephAlpha_luminous-base.json"
+        with open(input_file, 'r') as f:
+            data = json.load(f)
+        if len(data['request_states']) != max_lens[i]:
+            print("Warning: this model has less questions than max question, please change model.")
+        for j, question in enumerate(data['request_states']):
+            text = question['instance']['input']['text']
+            output_data.append([base_idx+j, text, bool_delete_list[base_idx+j]])
+        base_idx += max_lens[i]
+
+    output_df = pd.DataFrame(output_data, columns=["idx", "text", "is_deleted"])
+    output_df.to_csv(output_file, index=False)
+
+    all_responses_df.columns = [f'{i}' for i in range(all_responses_df.shape[1])]
+    all_responses_df.to_csv(f'{output_dir}/mask_matrix.csv', index_label=None)
+
+
+
+
+
+    
     
