ffe629c41bd69075ec6f48c542aaa3f24eb8bea8
yuhengtu <yuhengtuece@gmail.com>
2024-09-24 15:37:44 -0700

    nnew

--- src/fix_agg_embed_push_2.sh
+++ src/fix_agg_embed_push_2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NUM_AGENTS=25
+NUM_AGENTS=24
 HOSTNAME=$(hostname)
 
 for agent_num in $(seq 1 $NUM_AGENTS); do
-    nohup wandb agent yuhengtu/fix_agg_embed_push_2/mj31snqv > fix_agg_embed_push_2_${HOSTNAME}_${agent_num}.log 2>&1 &
+    nohup wandb agent yuhengtu/fix_agg_embed_push_2/r5ekbewk > fix_agg_embed_push_2_${HOSTNAME}_${agent_num}.log 2>&1 &
 done
