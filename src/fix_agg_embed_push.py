dce12782b58dcd69f150c65c9d5f37bdae0882c3
yuhengtu <yuhengtuece@gmail.com>
2024-09-24 14:21:47 -0700

    Create fix_agg_embed_push.py

---
+++ src/fix_agg_embed_push.py
@@ -0,0 +1,30 @@
+import argparse
+import wandb
+from utils import DATASETS
+from datasets import Dataset, DatasetDict
+import pandas as pd
+import os
+from huggingface_hub import login
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    wandb.init(project="fix_agg_embed_push")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True)
+    args = parser.parse_args()
+    
+    load_dotenv()
+    hf_token = os.getenv('HF_TOKEN')
+    login(token=hf_token)
+    
+    input_dir = f'../data/reeval-agg_embed_folder'
+    
+    agg_df = pd.concat(
+        [pd.read_csv(f'{input_dir}/embed_{dataset}.csv') for dataset in DATASETS],
+        ignore_index=True
+    )
+    agg_df['embed'] = agg_df['embed'].apply(lambda x: eval(x))
+    
+    agg_dataset = Dataset.from_pandas(agg_df)
+    dataset_dict = DatasetDict({'train': agg_dataset})
+    dataset_dict.push_to_hub("stair-lab/reeval_aggregate-embed")
