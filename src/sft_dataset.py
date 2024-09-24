e5ee199fdb6dafb58a9889d874d31ff83e3435b8
yuhengtu <yuhengtuece@gmail.com>
2024-09-23 22:07:31 -0700

    Update sft_dataset.py

--- src/sft_dataset.py
+++ src/sft_dataset.py
@@ -21,13 +21,13 @@ if __name__ == "__main__":
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": (
             """Generate question with a given difficulty, which range from -5 to 5. """
-            """The lower the score is, the more difficult the question is, """
+            """The lower the difficulty is, the more difficult the question is, """
             """hence a model is more likely to fail the questions. """
             """Output only the question and nothing else."""
             """Difficulty: "%s". Question: """
         )
         },
-        {"role": "assistant", "content": "%s"},
+        {"role": "assistant", "content": """\"%s\""""},
     ]
     
     template = tokenizer.apply_chat_template(
