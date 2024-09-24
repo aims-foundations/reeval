90d1d838b6679c3915051682b1d37def1553623c
yuhengtu <yuhengtuece@gmail.com>
2024-09-24 03:18:27 -0700

    Update sft_test.py

--- src/sft_test.py
+++ src/sft_test.py
@@ -1,23 +1,29 @@
 import torch
-from vllm import LLM, SamplingParams
+from vllm import LLM
 from peft import AutoPeftModelForCausalLM
-from datasets import load_dataset
+from transformers import AutoTokenizer
 
 if __name__ == "__main__":
     model_dir = "../data/sft"
     model = AutoPeftModelForCausalLM.from_pretrained(f'{model_dir}/checkpoint-2400')
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
     model = model.merge_and_unload().to(torch.bfloat16)
     model.save_pretrained(model_dir)
 
-    test_dataset = load_dataset("stair-lab/airbench-ppo", split="test")
-    prompts = test_dataset['text'][:5]
+    ppo_chat = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": (
+            """Generate a question with a given difficulty score, which range from -5 to 5. """
+            """The lower the score is, the more difficult the question is. """
+            """Hence a model is more likely to fail the questions. """
+            """Output only the question and nothing else. """
+            """Difficulty: 0.33. Question: """
+        )
+        },
+    ]
+    template = tokenizer.apply_chat_template(ppo_chat, tokenize=False, add_generation_prompt=True)
     
-    sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
     llm = LLM(model=model_dir)
-    outputs = llm.generate(prompts, sampling_params)
-    answers = [o.outputs[0].text for o in outputs]
-    
-    for prompt, answer in zip(prompts, answers):
-        print(f"Prompt: {prompt}")
-        print(f"Answer: {answer}")
-        print()
+    outputs = llm.generate(template)
+    print(f"Prompt: {template}")
+    print(f"Answer: {outputs.outputs[0].text}")
