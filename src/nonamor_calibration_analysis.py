5b132ab74e4a7263cb681faeafffbdb8854fc567
yuhengtu <yuhengtuece@gmail.com>
2024-09-28 01:23:42 -0700

    Update nonamor_calibration_analysis.py

--- src/nonamor_calibration_analysis.py
+++ src/nonamor_calibration_analysis.py
@@ -27,31 +27,31 @@ if __name__ == "__main__":
         theta_hat = pd.read_csv(f'{input_dir}/{dataset}/nonamor_theta.csv')['theta'].values
         z_hat = pd.read_csv(f'{input_dir}/{dataset}/nonamor_z.csv')['z'].values
         
-        gof_mean, gof_std = goodness_of_fit_1PL_plot(
-            z=torch.tensor(z_hat, dtype=torch.float32),
-            theta=torch.tensor(theta_hat, dtype=torch.float32),
-            y=torch.tensor(y, dtype=torch.float32),
-            plot_path=f"{plot_dir}/goodness_of_fit_{dataset}",
-        )
-        gof_means.append(gof_mean)
-        gof_stds.append(gof_std)
+        # gof_mean, gof_std = goodness_of_fit_1PL_plot(
+        #     z=torch.tensor(z_hat, dtype=torch.float32),
+        #     theta=torch.tensor(theta_hat, dtype=torch.float32),
+        #     y=torch.tensor(y, dtype=torch.float32),
+        #     plot_path=f"{plot_dir}/goodness_of_fit_{dataset}",
+        # )
+        # gof_means.append(gof_mean)
+        # gof_stds.append(gof_std)
         
-        corr_ctt_mean, corr_ctt_std = theta_corr_ctt_plot(
-            theta=theta_hat,
-            y=y,
-            plot_path=f"{plot_dir}/theta_corr_ctt_{dataset}",
-        )
-        corr_ctt_means.append(corr_ctt_mean)
-        corr_ctt_stds.append(corr_ctt_std)
+        # corr_ctt_mean, corr_ctt_std = theta_corr_ctt_plot(
+        #     theta=theta_hat,
+        #     y=y,
+        #     plot_path=f"{plot_dir}/theta_corr_ctt_{dataset}",
+        # )
+        # corr_ctt_means.append(corr_ctt_mean)
+        # corr_ctt_stds.append(corr_ctt_std)
         
-        if dataset != "airbench":
-            corr_helm_mean, corr_helm_std = theta_corr_helm_plot(
-                theta=theta_hat,
-                dataset=dataset,
-                plot_path=f"{plot_dir}/theta_corr_helm_{dataset}",
-            )
-            corr_helm_means.append(corr_helm_mean)
-            corr_helm_stds.append(corr_helm_std)
+        # if dataset != "airbench":
+        #     corr_helm_mean, corr_helm_std = theta_corr_helm_plot(
+        #         theta=theta_hat,
+        #         dataset=dataset,
+        #         plot_path=f"{plot_dir}/theta_corr_helm_{dataset}",
+        #     )
+        #     corr_helm_means.append(corr_helm_mean)
+        #     corr_helm_stds.append(corr_helm_std)
 
         plugin_train_indices = pd.read_csv(f'../data/plugin_regression/{dataset}/train_0.csv')['index'].values
         plugin_test_indices = pd.read_csv(f'../data/plugin_regression/{dataset}/test_0.csv')['index'].values
@@ -99,61 +99,61 @@ if __name__ == "__main__":
     })
     plugin_gof_df_test.to_csv(f'{plot_dir}/nonamor4plugin_gof_test.csv', index=False)
     
-    amor_gof_df_train = pd.DataFrame({
-        'datasets': DATASETS,
-        'gof_means': amor_gof_train_means,
-    })
-    amor_gof_df_train.to_csv(f'{plot_dir}/nonamor4amor_gof_train.csv', index=False)
+    # amor_gof_df_train = pd.DataFrame({
+    #     'datasets': DATASETS,
+    #     'gof_means': amor_gof_train_means,
+    # })
+    # amor_gof_df_train.to_csv(f'{plot_dir}/nonamor4amor_gof_train.csv', index=False)
     
-    amor_gof_df_test = pd.DataFrame({
-        'datasets': DATASETS,
-        'gof_means': amor_gof_test_means,
-    })
-    amor_gof_df_test.to_csv(f'{plot_dir}/nonamor4amor_gof_test.csv', index=False)
+    # amor_gof_df_test = pd.DataFrame({
+    #     'datasets': DATASETS,
+    #     'gof_means': amor_gof_test_means,
+    # })
+    # amor_gof_df_test.to_csv(f'{plot_dir}/nonamor4amor_gof_test.csv', index=False)
     
-    gof_df = pd.DataFrame({
-        'datasets': DATASETS,
-        'gof_means': gof_means,
-        'gof_stds': gof_stds
-    })
-    gof_df.to_csv(f'{plot_dir}/nonamor_calibration_gof.csv', index=False)
+    # gof_df = pd.DataFrame({
+    #     'datasets': DATASETS,
+    #     'gof_means': gof_means,
+    #     'gof_stds': gof_stds
+    # })
+    # gof_df.to_csv(f'{plot_dir}/nonamor_calibration_gof.csv', index=False)
     
-    ctt_df = pd.DataFrame({
-        'datasets': DATASETS,
-        'corr_ctt_means': corr_ctt_means,
-        'corr_ctt_stds': corr_ctt_stds
-    })
-    ctt_df.to_csv(f'{plot_dir}/nonamor_calibration_corr_ctt.csv', index=False)
+    # ctt_df = pd.DataFrame({
+    #     'datasets': DATASETS,
+    #     'corr_ctt_means': corr_ctt_means,
+    #     'corr_ctt_stds': corr_ctt_stds
+    # })
+    # ctt_df.to_csv(f'{plot_dir}/nonamor_calibration_corr_ctt.csv', index=False)
     
-    helm_df = pd.DataFrame({
-        'datasets': [d for d in DATASETS if d != "airbench"],
-        'corr_helm_means': corr_helm_means,
-        'corr_helm_stds': corr_helm_stds
-    })
-    helm_df.to_csv(f'{plot_dir}/nonamor_calibration_corr_helm.csv', index=False)
+    # helm_df = pd.DataFrame({
+    #     'datasets': [d for d in DATASETS if d != "airbench"],
+    #     'corr_helm_means': corr_helm_means,
+    #     'corr_helm_stds': corr_helm_stds
+    # })
+    # helm_df.to_csv(f'{plot_dir}/nonamor_calibration_corr_helm.csv', index=False)
 
-    error_bar_plot_single(
-        datasets=DATASETS,
-        means=gof_means,
-        stds=gof_stds,
-        plot_path=f"{plot_dir}/nonamor_calibration_summarize_gof",
-        xlabel=r"Goodness of Fit",
-        xlim_upper=0.5,
-    )
+    # error_bar_plot_single(
+    #     datasets=DATASETS,
+    #     means=gof_means,
+    #     stds=gof_stds,
+    #     plot_path=f"{plot_dir}/nonamor_calibration_summarize_gof",
+    #     xlabel=r"Goodness of Fit",
+    #     xlim_upper=0.5,
+    # )
     
-    error_bar_plot_single(
-        datasets=DATASETS,
-        means=corr_ctt_means,
-        stds=corr_ctt_stds,
-        plot_path=f"{plot_dir}/nonamor_calibration_summarize_theta_corr_ctt",
-        xlabel=r"$\theta$ correlation with CTT",
-    )
+    # error_bar_plot_single(
+    #     datasets=DATASETS,
+    #     means=corr_ctt_means,
+    #     stds=corr_ctt_stds,
+    #     plot_path=f"{plot_dir}/nonamor_calibration_summarize_theta_corr_ctt",
+    #     xlabel=r"$\theta$ correlation with CTT",
+    # )
     
-    error_bar_plot_single(
-        datasets=[d for d in DATASETS if d != "airbench"],
-        means=corr_helm_means,
-        stds=corr_helm_stds,
-        plot_path=f"{plot_dir}/nonamor_calibration_summarize_theta_corr_helm",
-        xlabel=r"$\theta$ correlation with HELM",
-    )
+    # error_bar_plot_single(
+    #     datasets=[d for d in DATASETS if d != "airbench"],
+    #     means=corr_helm_means,
+    #     stds=corr_helm_stds,
+    #     plot_path=f"{plot_dir}/nonamor_calibration_summarize_theta_corr_helm",
+    #     xlabel=r"$\theta$ correlation with HELM",
+    # )
     
