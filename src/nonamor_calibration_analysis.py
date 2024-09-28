08c66d6f40fdcb441d35d7908c2b6c4d6b2773da
yuhengtu <yuhengtuece@gmail.com>
2024-09-28 01:21:41 -0700

    Update nonamor_calibration_analysis.py

--- src/nonamor_calibration_analysis.py
+++ src/nonamor_calibration_analysis.py
@@ -8,6 +8,7 @@ from utils import (
     theta_corr_helm_plot,
     error_bar_plot_single,
     DATASETS,
+    goodness_of_fit_1PL
 )
 
 if __name__ == "__main__":
@@ -18,7 +19,8 @@ if __name__ == "__main__":
     gof_means, gof_stds = [], []
     corr_ctt_means, corr_ctt_stds = [], []
     corr_helm_means, corr_helm_stds = [], []
-    gof_train_means, gof_test_means = [], []
+    plugin_gof_train_means, plugin_gof_test_means = [], []
+    amor_gof_train_means, amor_gof_test_means = [], []
     for dataset in tqdm(DATASETS):
         print(f"Processing {dataset}")
         y = pd.read_csv(f'../data/pre_calibration/{dataset}/matrix.csv', index_col=0).values
@@ -51,7 +53,63 @@ if __name__ == "__main__":
             corr_helm_means.append(corr_helm_mean)
             corr_helm_stds.append(corr_helm_std)
 
-    # TODO: train and test gof
+        plugin_train_indices = pd.read_csv(f'../data/plugin_regression/{dataset}/train_0.csv')['index'].values
+        plugin_test_indices = pd.read_csv(f'../data/plugin_regression/{dataset}/test_0.csv')['index'].values
+        
+        plugin_gof_train_mean, _ = goodness_of_fit_1PL(
+            z=torch.tensor(z_hat[plugin_train_indices], dtype=torch.float32),
+            theta=torch.tensor(theta_hat, dtype=torch.float32),
+            y=torch.tensor(y[:, plugin_train_indices], dtype=torch.float32),
+        )
+        plugin_gof_train_means.append(plugin_gof_train_mean)
+        
+        plugin_gof_test_mean, _ = goodness_of_fit_1PL(
+            z=torch.tensor(z_hat[plugin_test_indices], dtype=torch.float32),
+            theta=torch.tensor(theta_hat, dtype=torch.float32),
+            y=torch.tensor(y[:, plugin_test_indices], dtype=torch.float32),
+        )
+        plugin_gof_test_means.append(plugin_gof_test_mean)
+        
+        amor_train_indices = pd.read_csv(f'../data/amor_calibration/{dataset}/train_0.csv')['index'].values
+        amor_test_indices = pd.read_csv(f'../data/amor_calibration/{dataset}/test_0.csv')['index'].values
+        
+        amor_gof_train_mean, _ = goodness_of_fit_1PL(
+            z=torch.tensor(z_hat[amor_train_indices], dtype=torch.float32),
+            theta=torch.tensor(theta_hat, dtype=torch.float32),
+            y=torch.tensor(y[:, amor_train_indices], dtype=torch.float32),
+        )
+        amor_gof_train_means.append(amor_gof_train_mean)
+        
+        amor_gof_test_mean, _ = goodness_of_fit_1PL(
+            z=torch.tensor(z_hat[amor_test_indices], dtype=torch.float32),
+            theta=torch.tensor(theta_hat, dtype=torch.float32),
+            y=torch.tensor(y[:, amor_test_indices], dtype=torch.float32),
+        )
+        amor_gof_test_means.append(amor_gof_test_mean)
+    
+    plugin_gof_df_train = pd.DataFrame({
+        'datasets': DATASETS,
+        'gof_means': plugin_gof_train_means,
+    })
+    plugin_gof_df_train.to_csv(f'{plot_dir}/nonamor4plugin_gof_train.csv', index=False)
+    
+    plugin_gof_df_test = pd.DataFrame({
+        'datasets': DATASETS,
+        'gof_means': plugin_gof_test_means,
+    })
+    plugin_gof_df_test.to_csv(f'{plot_dir}/nonamor4plugin_gof_test.csv', index=False)
+    
+    amor_gof_df_train = pd.DataFrame({
+        'datasets': DATASETS,
+        'gof_means': amor_gof_train_means,
+    })
+    amor_gof_df_train.to_csv(f'{plot_dir}/nonamor4amor_gof_train.csv', index=False)
+    
+    amor_gof_df_test = pd.DataFrame({
+        'datasets': DATASETS,
+        'gof_means': amor_gof_test_means,
+    })
+    amor_gof_df_test.to_csv(f'{plot_dir}/nonamor4amor_gof_test.csv', index=False)
     
     gof_df = pd.DataFrame({
         'datasets': DATASETS,
