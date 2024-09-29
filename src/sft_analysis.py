78f932da2efeb4d42c46f7e81c67af9684477d81
yuhengtu <yuhengtuece@gmail.com>
2024-09-28 17:05:58 -0700

    Update sft_analysis.py

--- src/sft_analysis.py
+++ src/sft_analysis.py
@@ -1,30 +1,64 @@
+import os
+import pandas as pd
 import torch
 from vllm import LLM, SamplingParams
 from peft import AutoPeftModelForCausalLM
 from transformers import AutoTokenizer
+from datasets import load_dataset, Dataset
+from utils import MLP, get_embed, plot_hist
+from ppo_reward_model import extract_score
+import pickle
 
 if __name__ == "__main__":
+    plot_dir = "../plot/sft"
+    os.makedirs(plot_dir, exist_ok=True)
+    
     model_dir = "../data/sft"
     model = AutoPeftModelForCausalLM.from_pretrained(f'{model_dir}/checkpoint-2400')
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
     model = model.merge_and_unload().to(torch.bfloat16)
     model.save_pretrained(model_dir)
-    sampling_params = SamplingParams(max_tokens=2048, temperature=0.6, top_p=0.9)
     
-    ppo_chat = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": (
-            """Generate a question with a given difficulty score, which range from -5 to 5. """
-            """The lower the score is, the more difficult the question is. """
-            """Hence a model is more likely to fail the questions. """
-            """Output only the question and nothing else. """
-            """Difficulty: 0.33. Question: """
-        )
-        },
-    ]
-    template = tokenizer.apply_chat_template(ppo_chat, tokenize=False, add_generation_prompt=True)
+    train_dataset = load_dataset("stair-lab/airbench-sft", split="train")
+    test_dataset = load_dataset("stair-lab/airbench-sft", split="test")
+    train_prompts = train_dataset['text']
+    test_prompts = test_dataset['text']
+    train_gt_zs = [extract_score(p) for p in train_prompts]
+    test_gt_zs = [extract_score(p) for p in test_prompts]
     
+    sampling_params = SamplingParams(temperature=0.6, top_p=0.9, max_tokens=256)
     llm = LLM(model=model_dir)
-    outputs = llm.generate(template, sampling_params)
-    print(f"Prompt: {template}")
-    print(f"Answer: {outputs[0].outputs[0].text}")
+    train_outputs = llm.generate(train_prompts, sampling_params)
+    test_outputs = llm.generate(test_prompts, sampling_params)
+    train_answers = [o.outputs[0].text for o in train_outputs]
+    test_answers = [o.outputs[0].text for o in test_outputs]
+    train_answer_df = pd.DataFrame(train_answers, columns=["text"])
+    test_answer_df = pd.DataFrame(test_answers, columns=["text"])
+    train_answer_dataset = Dataset.from_pandas(train_answer_df)
+    test_answer_dataset = Dataset.from_pandas(test_answer_df)
+    
+    del llm
+    torch.cuda.empty_cache()
+    
+    answer_train_embs = get_embed(train_answer_dataset)
+    answer_test_embs = get_embed(test_answer_dataset)
+    
+    with open('../data/plugin_regression/airbench/bayridge.pkl', 'rb') as f:
+        reward_model = pickle.load(f)
+    train_pred_zs = reward_model.predict(answer_train_embs).tolist()
+    test_pred_zs = reward_model.predict(answer_test_embs).tolist()
+    
+    train_diffs = [abs(a - b) for a, b in zip(train_pred_zs, train_gt_zs)]
+    test_diffs = [abs(a - b) for a, b in zip(test_pred_zs, test_gt_zs)]
+    
+    plot_hist(
+        data=train_diffs,
+        plot_path=f"{plot_dir}/sft_diff_hist_train.png",
+        ylabel=r"train $z$ difference",
+    )
+    
+    plot_hist(
+        data=test_diffs,
+        plot_path=f"{plot_dir}/sft_diff_hist_test.png",
+        ylabel=r"test $z$ difference",
+    )
