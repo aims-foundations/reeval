659f7c3b765e0a7559a84ebfe045ec9f661950e3
yuhengtu <yuhengtuece@gmail.com>
2024-09-28 23:47:22 -0700

    new

--- src/agg_calibration.py
+++ src/agg_calibration.py
@@ -34,7 +34,7 @@ def agg_amor_calibration(
     emb_hf_repo: str,
     model_id_path: str,
     lr_theta=0.01,
-    lr_mlp=1e-3,
+    lr_mlp=1e-5,
     max_epoch=10,
     embed_dim=4096,
     bs=4096,
