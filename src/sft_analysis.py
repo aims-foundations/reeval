5f40eba3866e387277a110f87d2d8ac116cdf71f
yuhengtu <yuhengtuece@gmail.com>
2024-09-30 03:16:36 -0700

    Update sft_analysis.py

--- src/sft_analysis.py
+++ src/sft_analysis.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     os.makedirs(plot_dir, exist_ok=True)
     
     if args.llm == "llama":
-        model_dir = "../data/sft/lora_20epoch"
-        model = AutoPeftModelForCausalLM.from_pretrained(f'{model_dir}/checkpoint-4800')
+        model_dir = "../data/sft/lora_10epoch"
+        model = AutoPeftModelForCausalLM.from_pretrained(f'{model_dir}/checkpoint-2400')
         train_dataset = load_dataset("stair-lab/airbench-sft", split="train")
         test_dataset = load_dataset("stair-lab/airbench-sft", split="test")
         train_prompts = train_dataset['text']
@@ -30,12 +30,14 @@ if __name__ == "__main__":
     train_gt_zs = [extract_score(p) for p in train_prompts]
     test_gt_zs = [extract_score(p) for p in test_prompts]
     
-    sampling_params = SamplingParams(temperature=0.6, top_p=0.9, max_tokens=256)
+    sampling_params = SamplingParams(temperature=0.6, n=64, best_of_n=128, top_p=0.9, max_tokens=256)
     llm = LLM(model=model_dir)
     train_outputs = llm.generate(train_prompts, sampling_params)
     test_outputs = llm.generate(test_prompts, sampling_params)
-    train_answers = [o.outputs[0].text for o in train_outputs]
-    test_answers = [o.outputs[0].text for o in test_outputs]
+    # batch x n_samples
+    
+    train_answers = [sample.text for o in train_outputs for sample in o.outputs]
+    test_answers = [sample.text for o in test_outputs for sample in o.outputs]
     train_answer_df = pd.DataFrame(train_answers, columns=["text"])
     test_answer_df = pd.DataFrame(test_answers, columns=["text"])
     train_answer_dataset = Dataset.from_pandas(train_answer_df)
@@ -44,16 +46,22 @@ if __name__ == "__main__":
     del llm
     torch.cuda.empty_cache()
     
-    answer_train_embs = get_embed(train_answer_dataset)
-    answer_test_embs = get_embed(test_answer_dataset)
+    # Perfrom batch inference
+    def call_diff(ds, gt_zs, reward_model):
+        output_diffs = []
+        ds_loader = torch.utils.data.DataLoader(ds, batch_size=64, shuffle=False)
+        for batch in ds_loader:
+            embs = get_embed(batch)
+            pred_zs = reward_model.predict(embs).tolist()
+            diffs = [abs(a - b) for a, b in zip(pred_zs, gt_zs)]
+            output_diffs.extend(diffs)
+        return diffs
     
     with open('../data/plugin_regression/airbench/bayridge.pkl', 'rb') as f:
         reward_model = pickle.load(f)
-    train_pred_zs = reward_model.predict(answer_train_embs).tolist()
-    test_pred_zs = reward_model.predict(answer_test_embs).tolist()
-    
-    train_diffs = [abs(a - b) for a, b in zip(train_pred_zs, train_gt_zs)]
-    test_diffs = [abs(a - b) for a, b in zip(test_pred_zs, test_gt_zs)]
+        
+    train_diffs = call_diff(train_answer_dataset, train_gt_zs, reward_model)
+    test_diffs = call_diff(test_answer_dataset, test_gt_zs, reward_model)
     
     plot_hist(
         data=train_diffs,
