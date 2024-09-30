7e621904ab35eecf97014e44301b6104353d3be6
yuhengtu <yuhengtuece@gmail.com>
2024-09-30 14:30:13 -0700

    Update agg_amor_calibration_bydataset.py

--- src/agg_amor_calibration_bydataset.py
+++ src/agg_amor_calibration_bydataset.py
@@ -86,6 +86,7 @@ def agg_amor_calibration(
             
             train_losses = []
             z_batch_train = []
+            gt_batch_train = []
             total_z_mse_train = 0
             total_loss_train = 0
             for emb_batch, y_batch, gt_z_train_batch in tqdm(data_loader, desc='Batch'):
@@ -120,7 +121,7 @@ def agg_amor_calibration(
                 theta_train_subset = theta_train_subset.detach()
                 if epoch == max_epoch-1:
                     z_batch_train.extend(list(z_train.detach().cpu().numpy()))
-                    gt_batch_train = gt_z_train_batch.detach().cpu().numpy()
+                    gt_batch_train.extend(list(gt_z_train_batch.detach().cpu().numpy()))
             
             wandb.log({'train_loss': total_loss_train/len(data_loader)})
             wandb.log({'mse_z_train': total_z_mse_train.item()/gt_z_train.shape[0]})
