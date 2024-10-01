ae0157c4ed94fb7f8e25beb19b93edd74906f488
yuhengtu <yuhengtuece@gmail.com>
2024-09-30 22:19:02 -0700

    Create hard_easy_test_sweep.sh

---
+++ src/hard_easy_test_sweep.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NUM_AGENTS=29
+HOSTNAME=$(hostname)
+
+for agent_num in $(seq 2 $((NUM_AGENTS))); do
+    nohup wandb agent ura-hcmut/hard_easy_test_sweep/4la3ktxw > hard_easy_test_sweep_${HOSTNAME}_${agent_num}.log 2>&1 &
+done
