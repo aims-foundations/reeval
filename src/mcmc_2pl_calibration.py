fe456998819840e8e30bc5a5ac3e47e6522dc719
yuhengtu <yuhengtuece@gmail.com>
2024-10-13 07:42:01 +0800

    new

--- src/mcmc_2pl_calibration.py
+++ src/mcmc_2pl_calibration.py
@@ -27,7 +27,7 @@ def model(question_num, testtaker_num, response_matrix):
     
     numpyro.sample("obs", dist.Bernoulli(prob_matrix), obs=response_matrix)
 
-def irt_mcmc(question_num, testtaker_num, response_matrix, num_samples=9000, num_warmup=1000):
+def irt_mcmc(question_num, testtaker_num, response_matrix, num_samples=2000, num_warmup=1000):
     rng_key = random.PRNGKey(0)
     rng_key, rng_key_ = random.split(rng_key)
     
