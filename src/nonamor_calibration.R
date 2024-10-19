4d38fe88439417c3ab5fdb9d6eb68afe990e733d
yuhengtu <yuhengtuece@gmail.com>
2024-10-19 19:53:16 +0800

    new

--- src/nonamor_calibration.R
+++ src/nonamor_calibration.R
@@ -3,17 +3,20 @@ library(ggplot2)
 library(dplyr)
 
 input_file <- "../data/pre_calibration/mmlu/matrix.csv"
-output_files <- c("../nonamor_calibration_R/mmlu/R_1PL.csv", 
-                  "../nonamor_calibration_R/mmlu/R_2PL.csv", 
-                  "../nonamor_calibration_R/mmlu/R_3PL.csv")
+z_files <- c("../data/nonamor_calibration_R/mmlu/R_1PL_z.csv", 
+                  "../data/nonamor_calibration_R/mmlu/R_2PL_z.csv", 
+                  "../data/nonamor_calibration_R/mmlu/R_3PL_z.csv")
+theta_files <- c("../data/nonamor_calibration_R/mmlu/R_1PL_theta.csv", 
+                  "../data/nonamor_calibration_R/mmlu/R_2PL_theta.csv", 
+                  "../data/nonamor_calibration_R/mmlu/R_3PL_theta.csv")
 
 models <- c('Rasch', '2PL', '3PL')
-
 data <- read.csv(input_file, row.names=1)
 
 for (j in 1:length(models)) {
   model <- mirt(data, 1, models[j])
-  coefficient <- coef(model)
-  write.csv(coefficient, output_files[j])
-  fscores(model)
+  z <- coef(model)
+  write.csv(z, z_files[j])
+  theta <- fscores(model)
+  write.csv(theta, theta_files[j])
 }
--- src/nonamor_calibration_R.py
+++ src/nonamor_calibration_R.py
@@ -1,10 +1,9 @@
 import argparse
 import os
 import subprocess
-import wandb
+import pandas as pd
 
 if __name__ == "__main__":
-    wandb.init(project="cat")
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, required=True)
     args = parser.parse_args()
@@ -15,11 +14,9 @@ if __name__ == "__main__":
     subprocess.run(f"conda run -n cat Rscript nonamor_calibration.R", shell=True, check=True)
     
     for filename in os.listdir(output_dir):
-        if filename.endswith('.csv'):
-            file_path = os.path.join(Z_dir, filename)
+        if filename.endswith('_z.csv'):
+            file_path = f'{output_dir}/{filename}'
             df = pd.read_csv(file_path)
-
-            # Delete columns
             df = df.iloc[:, 1:-2]
 
             # Define new columns and prepare data
@@ -29,11 +26,9 @@ if __name__ == "__main__":
                 for col, new_col in zip(df.columns[i:i+4], new_columns):
                     data[new_col].append(df[col].values[0])
 
-            # Create a new DataFrame with the cleaned data
             new_df = pd.DataFrame(data)
             new_df = new_df[['z1', 'z2', 'z3']]
 
-            # Save the cleaned data to a new CSV file
-            clean_file_path = os.path.join(Z_dir, filename.replace('.csv', '_clean.csv'))
+            clean_file_path = f"{output_dir}/{filename.replace('.csv', '_clean.csv')}"
             new_df.to_csv(clean_file_path, index=False)
 
