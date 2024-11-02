72b2acd32936e234ca55cec7ae3c4bc24abf7d22
yuhengtu <yuhengtuece@gmail.com>
2024-11-03 03:02:17 +0800

    Update mle_multi_dim_amor_theta.py

--- src/mle_multi_dim_amor_theta.py
+++ src/mle_multi_dim_amor_theta.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
     # valid_datasets_df.to_csv(f"{output_dir}/valid_datasets.csv", index=False)
     
     combined_matrix = pd.read_csv(f"{output_dir}/combined_matrix.csv", index_col=0)
-    valid_datasets = pd.read_csv(f"{output_dir}/valid_datasets.csv", index_col=0).values
+    valid_datasets = pd.read_csv(f"{output_dir}/valid_datasets.csv").values
     
     # W, b, a, z_hat = mle_multi_dim_amor_theta(
     #     response_matrix=torch.tensor(combined_matrix.values, dtype=torch.float32),
