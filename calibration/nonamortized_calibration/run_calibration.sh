578f549f13c3e04245ee881511cbeb392aba2eaa
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-11-18 02:01:07 -0800

    Factorize code

--- calibration/nonamortized_calibration/run_calibration.sh
+++ calibration/nonamortized_calibration/run_calibration.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+wandb sweep calibration.yaml
+CUDA_VISIBLE_DEVICES=5 wandb agent ura-hcmut/calibration/ixc900c7 &
+CUDA_VISIBLE_DEVICES=4 wandb agent ura-hcmut/calibration/ixc900c7 &
+CUDA_VISIBLE_DEVICES=6 wandb agent ura-hcmut/calibration/ixc900c7 &
+CUDA_VISIBLE_DEVICES=7 wandb agent ura-hcmut/calibration/ixc900c7 &
+wandb agent ura-hcmut/calibration/ixc900c7
+wandb agent ura-hcmut/calibration/ixc900c7
+
 # List of dataset names
 datasets=(
     "airbench"
@@ -26,33 +35,28 @@ datasets=(
     "lsat_qa"
     "bold"
     "dyck_language_np3"
+    "combined_data"
 )
 
-# Loop through each dataset and run the Python command
-# for dataset in "${datasets[@]}"; do
-#     echo "Calibrating $dataset 1PL"
-#     python calibrate.py --dataset $dataset --PL 1 --fitting_method mle& 
-# done
-
-# for dataset in "${datasets[@]}"; do
-#     echo "Calibrating $dataset 2PL"
-#     python calibrate.py --dataset $dataset --PL 2 --fitting_method mle& 
-# done
-
-# python calibration_analysis.py --PL 1 --fitting_method mle
-# python calibration_analysis.py --PL 1 --fitting_method mle
-
-
-# Loop through each dataset and run the Python command
+# Loop through each dataset
 for dataset in "${datasets[@]}"; do
-    echo "Calibrating $dataset 1PL"
-    python calibrate.py --dataset $dataset --PL 1 --fitting_method em& 
+    # Lopp through each PL
+    for PL in 1 2 3; do
+        # Loop through each dimension d
+        for d in 1 2; do
+            # Loop through each fitting method
+            for fitting_method in mle em;
+                # Loop through whether to use amortization on question
+                for amortize_question in True False; do
+                    echo "Calibrating $dataset $PL $d $fitting_method $amortize_question"
+                    python calibrate.py --dataset $dataset --PL $PL --d $d --fitting_method $fitting_method --amortize_question $amortize_question &
+                done
+            done
+        done
+    done
 done
 
-# for dataset in "${datasets[@]}"; do
-#     echo "Calibrating $dataset 2PL"
-#     python calibrate.py --dataset $dataset --PL 2 --fitting_method em& 
-# done
-
+# python calibration_analysis.py --PL 1 --fitting_method mle
+# python calibration_analysis.py --PL 1 --fitting_method mle
 # python calibration_analysis.py --PL 1 --fitting_method em
 # python calibration_analysis.py --PL 1 --fitting_method em
--- cond_gen/sft_analysis.py
+++ cond_gen/sft_analysis.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import torch
 from datasets import Dataset, load_dataset
-from embed_text_package.embed_text import Embedder
+from embed_text_package.embed_text_v2 import Embedder
 from ppo_reward_model import extract_score
 from transformers import GenerationConfig
 from utils.utils import plot_hist
@@ -19,13 +19,15 @@ def call_diff(ds, gt_zs, reward_model, restart):
     embs = emb["text"]
 
     pred_zs = reward_model.predict(embs).tolist()
-    pred_zs = np.array(pred_zs).reshape(-1, restart)
+    pred_zs = torch.tensor(pred_zs).reshape(-1, restart)
     # >>> batch_size * restart
 
-    gt_zs = np.array([gt_zs for _ in range(restart)]).T
+    # gt_zs = np.array([gt_zs for _ in range(restart)]).T
+    gt_zs_tensor = torch.tensor(gt_zs)[:, None].expand(-1, restart)
 
-    best_diffs = np.abs(pred_zs - gt_zs).min(axis=-1)
-    return best_diffs
+    best_diffs = torch.abs(pred_zs - gt_zs_tensor).min(dim=1).values
+    
+    return best_diffs.cpu()
 
 
 if __name__ == "__main__":
@@ -35,14 +37,14 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    plot_dir = "../plot/sft"
+    plot_dir = "../plot/sft_new"
     os.makedirs(plot_dir, exist_ok=True)
     restart = 64
 
-    train_dataset = load_dataset("stair-lab/airbench-sft", split="train")
-    test_dataset = load_dataset("stair-lab/airbench-sft", split="test")
-    train_prompts = train_dataset["text"]
-    test_prompts = test_dataset["text"]
+    train_dataset = load_dataset("stair-lab/airbench-ppo", split="train")
+    test_dataset = load_dataset("stair-lab/airbench-ppo", split="test")
+    train_prompts = train_dataset["text"][:5]
+    test_prompts = test_dataset["text"][:5]
 
     train_gt_zs = [extract_score(p) for p in train_prompts]
     test_gt_zs = [extract_score(p) for p in test_prompts]
@@ -50,16 +52,24 @@ if __name__ == "__main__":
     generation_config = GenerationConfig.from_pretrained(args.model)
     sampling_params = SamplingParams(
         n=restart,
-        best_of=2 * restart,
-        temperature=generation_config.temperature,
+        best_of=restart,
+        temperature=1, # generation_config.temperature,
         top_p=generation_config.top_p,
-        max_tokens=2048,
+        max_tokens=128,
+        stop_token_ids=[128001, 128009],
     )
-    llm = LLM(model=args.model, gpu_memory_utilization=0.7)
+    llm = LLM(
+        model=args.model,
+        tensor_parallel_size=torch.cuda.device_count(),
+        gpu_memory_utilization=0.9,
+        dtype=torch.float16,
+    )
 
     train_outputs = llm.generate(train_prompts, sampling_params)
+    # >>> batch_size
     test_outputs = llm.generate(test_prompts, sampling_params)
-
+    # >>> batch_size
+    
     train_answers = [sample.text for o in train_outputs for sample in o.outputs]
     test_answers = [sample.text for o in test_outputs for sample in o.outputs]
     train_answer_df = pd.DataFrame(train_answers, columns=["text"])
@@ -71,14 +81,17 @@ if __name__ == "__main__":
     torch.cuda.empty_cache()
 
     embdr = Embedder()
-    embdr.load("meta-llama/Meta-Llama-3-8B")
+    embdr.load("meta-llama/Meta-Llama-3-8B", 
+               dtype=torch.float16,
+               tensor_parallel_size=torch.cuda.device_count(),
+               )
 
     with open("../data/plugin_regression/airbench/bayridge.pkl", "rb") as f:
         reward_model = pickle.load(f)
 
     train_diffs = call_diff(train_answer_dataset, train_gt_zs, reward_model, restart)
     test_diffs = call_diff(test_answer_dataset, test_gt_zs, reward_model, restart)
-
+    breakpoint()
     plot_hist(
         data=train_diffs,
         plot_path=f"{plot_dir}/sft_diff_hist_train_20epoch.png",
