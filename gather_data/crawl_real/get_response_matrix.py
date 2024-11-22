32cb8ada02c8feda95992945496cf4bc22ddae02
yuhengtu <yuhengtuece@gmail.com>
2024-11-23 02:17:26 +0800

    push_mmlu

--- gather_data/crawl_real/get_response_matrix.py
+++ gather_data/crawl_real/get_response_matrix.py
@@ -109,9 +109,6 @@ if __name__ == "__main__":
         f for f in full_strings_all if (f.split(":")[0].split(",")[0] == args.dataset)
     ]
     
-    print(full_strings)
-    
-    print(args.dataset)
     if args.dataset == "civil_comments":
         full_strings = [f for f in full_strings if "groups=ablation_prompts" not in f \
             and "groups=ablation_in_context" not in f]
