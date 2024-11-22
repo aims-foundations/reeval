dea1094eae7c5a55a9801fbe544e505d03bdb9da
yuhengtu <yuhengtuece@gmail.com>
2024-11-23 03:11:31 +0800

    misc

--- gather_data/pushing_scripts/push_mmlu.py
+++ gather_data/pushing_scripts/push_mmlu.py
@@ -5,9 +5,9 @@ upload_api = HfApi()
 for file in os.listdir(f"/dfs/scratch1/nqduc/reeval/data/gather_data/crawl_real/jsons/mmlu_json"):
     mmlu_file = f"/dfs/scratch1/nqduc/reeval/data/gather_data/crawl_real/jsons/mmlu_json/{file}"
     upload_api.upload_file(
-                repo_id="stair-lab/reeval_jsons",
-                repo_type="dataset",
-                path_in_repo=f"jsons/mmlu_json/{file}",
-                path_or_fileobj=mmlu_file,
-                # run_as_future=True,
-            )
+        repo_id="stair-lab/reeval_jsons",
+        repo_type="dataset",
+        path_in_repo=f"jsons/mmlu_json/{file}",
+        path_or_fileobj=mmlu_file,
+        # run_as_future=True,
+    )
