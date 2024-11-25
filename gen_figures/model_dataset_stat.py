453528e16432e18e53b7beef86855fb32351cb6c
yuhengtu <yuhengtuece@gmail.com>
2024-11-25 16:43:24 +0800

    model dataset stat plot

--- gen_figures/model_dataset_stat.py
+++ gen_figures/model_dataset_stat.py
@@ -1,7 +1,10 @@
-from huggingface_hub import snapshot_download
 import pandas as pd
 from tqdm import tqdm
-from utils.constants import DATASETS
+from utils.constants import PLOT_NAME_MAP, DATASETS
+from huggingface_hub import snapshot_download
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     DATASETS = sorted(DATASETS)
@@ -12,24 +15,28 @@ if __name__ == "__main__":
     for dataset in tqdm(DATASETS):
         model_name = pd.read_csv(f"{data_folder}/{dataset}/matrix.csv", index_col=0).index.tolist()
         model_names.extend(model_name)
-    print(list(set(model_names)))
     model_names = sorted(list(set(model_names)))
     
-    # create a dataframe, each row is a model, each column is a dataset, the value is 1 if the model is in the dataset
+    # create a dataframe, each row is a model, each column is a dataset, 
+    # the value is 1 if the model is in the dataset
     df = pd.DataFrame(0, index=model_names, columns=DATASETS)
     for dataset in DATASETS:
         model_name = pd.read_csv(f"{data_folder}/{dataset}/matrix.csv", index_col=0).index.tolist()
         df.loc[model_name, dataset] = 1
-    df.to_csv(f"{data_folder}/model_dataset_stat.csv")
-    
-    # generate a plot, turn 1 into checkmark
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    df = pd.read_csv(f"{data_folder}/model_dataset_stat.csv", index_col=0)
-    df = df.replace(1, "✔")
-    plt.figure(figsize=(20, 10))
-    sns.heatmap(df, cmap="coolwarm", cbar=False)
-    plt.savefig(f"{data_folder}/model_dataset_stat.png")
-    plt.show()
-    
-    
+    df.columns = [PLOT_NAME_MAP.get(col, col) for col in df.columns]
+        
+    output_dir = "../plot/model_dataset_stat"
+    os.makedirs(output_dir, exist_ok=True)
+    df.to_csv(f"{output_dir}/model_dataset_stat.csv")
+
+    data_transposed = df.T
+    plt.figure(figsize=(40, 30))
+    plt.imshow(data_transposed, cmap='Blues', interpolation='nearest')
+    plt.xticks(range(data_transposed.shape[1]), data_transposed.columns, rotation=45, ha='right', fontsize=6)
+    plt.yticks(range(data_transposed.shape[0]), data_transposed.index, fontsize=6)
+    plt.grid(visible=True, which='both', color='gray', linestyle='--', linewidth=0.5)
+    plt.gca().set_xticks([x - 0.5 for x in range(1, data_transposed.shape[1])], minor=True)
+    plt.gca().set_yticks([y - 0.5 for y in range(1, data_transposed.shape[0])], minor=True)
+    plt.gca().grid(which='minor', color='gray', linestyle='-', linewidth=0.5)
+    plt.savefig(f"{output_dir}/model_dataset_stat.png", dpi=300, bbox_inches='tight')
+    plt.close()
