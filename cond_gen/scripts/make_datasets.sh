2721dafc4d71bf536197cbb8843923b8a8e3c33d
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-11-27 02:19:28 -0800

    Add runscripts

---
+++ cond_gen/scripts/make_datasets.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# List of dataset names
+datasets=(
+    # "airbench"
+    "twitter_aae"
+    # "math"
+    "entity_data_imputation"
+    "real_toxicity_prompts"
+    "civil_comments"
+    "imdb"
+    "boolq"
+    "wikifact"
+    "babi_qa"
+    "mmlu"
+    "truthful_qa"
+    "legal_support"
+    "synthetic_reasoning"
+    "quac"
+    "entity_matching"
+    "synthetic_reasoning_natural"
+    "bbq"
+    "raft"
+    "narrative_qa"
+    "commonsense"
+    "lsat_qa"
+    "bold"
+    "dyck_language_np3"
+)
+
+for dataset in "${datasets[@]}"; do
+    echo "Running $dataset"
+    python 1_sft_dataset.py --dataset $dataset --model mistralai/Mistral-7B-Instruct-v0.3
+done
