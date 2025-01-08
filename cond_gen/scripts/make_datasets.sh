eb4484ff5dc911db28c50687e40d7dbad8c8a622
Martin Nguyen <nguyenquangduc2000@gmail.com>
2025-01-08 15:15:10 -0800

    Update dataset creating and analysis scripts

--- cond_gen/scripts/make_datasets.sh
+++ cond_gen/scripts/make_datasets.sh
@@ -2,36 +2,47 @@
 
 # List of dataset names
 datasets=(
-    "airbench"
-    "twitter_aae"
-    "math"
-    "entity_data_imputation"
-    "real_toxicity_prompts"
-    "civil_comments"
-    "imdb"
-    "boolq"
-    "wikifact"
-    "babi_qa"
-    "mmlu"
-    "truthful_qa"
-    "legal_support"
-    "synthetic_reasoning"
-    "quac"
-    "entity_matching"
-    "synthetic_reasoning_natural"
-    "bbq"
-    "raft"
-    "narrative_qa"
-    "commonsense"
-    "lsat_qa"
-    "bold"
-    "dyck_language_np3"
-    "thai_exam"
+    "air-bench/air_bench_2024"
+    "classic/babi_qa"
+    "classic/bbq"
+    # "classic/blimp"
+    "classic/bold"
+    "classic/boolq"
+    "classic/civil_comments"
+    "classic/code"
+    "classic/commonsense"
+    # "classic/copyright"
+    # "classic/disinfo"
+    "classic/dyck_language_np=3"
+    "classic/entity_data_imputation"
+    "classic/entity_matching"
+    "classic/gsm"
+    # "classic/ice"
+    "classic/imdb"
+    "classic/legal_support"
+    "classic/lsat_qa"
+    "classic/math"
+    "classic/mmlu"
+    "classic/msmarco"
+    "classic/narrative_qa"
+    # "classic/natural_qa"
+    "classic/quac"
+    "classic/raft"
+    "classic/real_toxicity_prompts"
+    # "classic/summarization_cnndm"
+    "classic/summarization_xsum"
+    "classic/synthetic_reasoning"
+    "classic/synthetic_reasoning_natural"
+    # "classic/the_pile"
+    "classic/truthful_qa"
+    "classic/twitter_aae"
+    "classic/wikifact"
+    "thaiexam/thai_exam"
 )
 
 for dataset in "${datasets[@]}"; do
     echo "Running $dataset"
-    python 1_sft_dataset.py --dataset $dataset --model meta-llama/Meta-Llama-3-8B
+    python 1_sft_dataset.py --dataset $dataset --model meta-llama/Meta-Llama-3.1-8B-Instruct
 done
 
 for dataset in "${datasets[@]}"; do
@@ -41,5 +52,10 @@ done
 
 for dataset in "${datasets[@]}"; do
     echo "Creating PPO dataset $dataset"
-    python ppo_dataset.py --dataset $dataset
+    python ppo_dataset.py --dataset $dataset --model meta-llama/Meta-Llama-3.1-8B-Instruct
+done
+
+for dataset in "${datasets[@]}"; do
+    echo "Creating PPO dataset $dataset"
+    python ppo_dataset.py --dataset $dataset --model mistralai/Mistral-7B-Instruct-v0.3
 done
