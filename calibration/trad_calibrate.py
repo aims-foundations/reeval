ad01747fe4ac143f760b91e45a41c476009f6d23
Yuheng Tu <yuhengtu@skampere1.stanford.edu>
2025-02-20 10:39:32 -0800

    gather data and trad calibrate

---
+++ calibration/trad_calibrate.py
@@ -0,0 +1,139 @@
+import os
+from tqdm import tqdm
+import pandas as pd
+import argparse
+import torch
+import numpy as np
+from copy import deepcopy
+from torchmetrics.functional import spearman_corrcoef
+from torch.distributions import Bernoulli
+from huggingface_hub import HfApi, snapshot_download
+
+scenario2metric = {
+    "thai_exam": "exact_match",
+    "mmlu": "exact_match",
+    "babi_qa": "quasi_exact_match",
+    "bbq": "quasi_exact_match",
+    "blimp": "exact_match",
+    # "bold": "toxic_frac",
+    "boolq": "quasi_exact_match",
+    "civil_comments": "quasi_exact_match",
+    # "code": "test_avg",
+    "commonsense": "exact_match",
+    # "copyright": "edit_distance",
+    # "disinfo": "self_bleu",
+    "dyck_language_np=3": "exact_match_indicator",
+    "entity_data_imputation": "quasi_exact_match",
+    "entity_matching": "quasi_exact_match",
+    "gsm": "exact_match_indicator",
+    # "ice": "logprob",
+    "imdb": "quasi_exact_match",
+    "legal_support": "quasi_exact_match",
+    "lsat_qa": "exact_match",
+    # "math": ["math_equiv", "math_equiv_chain_of_thought"],
+    "mmlu": "exact_match",
+    # "msmarco": ["RR@10", "NDCG@10"],
+    # "narrative_qa": "f1_score",
+    # "natural_qa": "f1_score",
+    # "quac": "f1_score",
+    # "raft": "quasi_exact_match",
+    # "real_toxicity_prompts": "toxic_frac",
+    # "summarization_cnndm": "rouge_l",
+    # "summarization_xsum": "rouge_l",
+    # "synthetic_efficiency": "inference_runtime",
+    "synthetic_reasoning": "quasi_exact_match",
+    # "synthetic_reasoning_natural": "f1_set_match",
+    # "the_pile": "logprob",
+    "truthful_qa": "exact_match",
+    # "twitter_aae": "logprob",
+    "wikifact": "quasi_exact_match",
+}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--scenario", type=str, required=True)
+    args = parser.parse_args()
+    
+    torch.manual_seed(0)
+    
+    # data_folder = snapshot_download(repo_id="stair-lab/reeval_another_repo", repo_type="dataset")
+    data_folder = f"../data/another_repo"
+    data = pd.read_parquet(f"{data_folder}/{args.scenario}/data.parquet", engine="fastparquet")
+    metric = scenario2metric[args.scenario]
+    # create a pivot table with model_id as index, instance_id as columns, and exact_match as values
+    raw_matrix = data.pivot(index="model_id", columns="instance_id", values=metric)
+    # drop the columns with only (0 and nan) or (1 and nan)
+    invalid_instance_ids = raw_matrix.columns[raw_matrix.isin([0, np.nan]).all() | raw_matrix.isin([1, np.nan]).all()].values
+    matrix = raw_matrix.drop(columns=invalid_instance_ids)
+    matrix = torch.tensor(matrix.values).float()
+    print(f"scenario: {args.scenario}, before drop: {raw_matrix.shape}, after drop: {matrix.shape}")
+    n_test_takers, n_items = matrix.shape
+    # TODO: better way is 80% of non-missing data turn into nan, also assert no one column/row is pure nan
+    train_mask = torch.bernoulli(torch.full((n_test_takers, n_items), 0.8)).bool()
+    train_matrix = matrix.clone()
+    train_matrix[~train_mask] = torch.nan
+    save_dir = f"../data/trad_calibrate/{args.scenario}"
+    os.makedirs(save_dir, exist_ok=True)
+    torch.save(train_mask, f"{save_dir}/train_mask.pt")
+
+    # fit z
+    def closure():
+        optim.zero_grad()
+        probs = torch.sigmoid(thetas[:, :, None] + z[None, None, :])
+        mask = ~torch.isnan(train_matrix)[None, :, :].repeat(500, 1, 1)
+        repeat_train_matrix = train_matrix[None, :, :].repeat(500, 1, 1)
+        loss = -Bernoulli(probs=probs[mask]).log_prob(repeat_train_matrix[mask]).mean()
+        loss.backward()
+        return loss
+    z = torch.zeros(n_items, requires_grad=True)
+    optim = torch.optim.LBFGS([z], lr=0.1, max_iter=20, history_size=10, line_search_fn="strong_wolfe")
+    thetas = torch.randn(500, n_test_takers)
+
+    pbar = tqdm(range(100))
+    for iteration in pbar:
+        if iteration > 0:
+            previous_z = z.clone()
+            previous_loss = loss.clone()
+        
+        loss = optim.step(closure)
+        
+        if iteration > 0:
+            d_loss = previous_loss - loss
+            d_z = torch.norm(previous_z - z, p=2)
+            grad_norm = torch.norm(optim.param_groups[0]["params"][0].grad, p=2)
+            pbar.set_postfix({"grad_norm": grad_norm, "d_z": d_z, "d_loss": d_loss})
+            if d_loss < 1e-5 and d_z < 1e-5 and grad_norm < 1e-5:
+                break
+    
+    torch.save(z, f"{save_dir}/z.pt")
+    print("z spearman with CTT: ", spearman_corrcoef(z, matrix.mean(0)))
+    
+     # fit theta
+    def closure():
+        optim.zero_grad()
+        probs = torch.sigmoid(theta[:, None] + z[None, :])
+        mask = ~torch.isnan(train_matrix)
+        loss = -Bernoulli(probs=probs[mask]).log_prob(train_matrix[mask]).mean()
+        loss.backward()
+        return loss
+    theta = torch.zeros(n_test_takers, requires_grad=True)
+    optim = torch.optim.LBFGS([theta], lr=0.1, max_iter=20, history_size=10, line_search_fn="strong_wolfe")
+    
+    pbar = tqdm(range(100))
+    for iteration in pbar:
+        if iteration > 0:
+            previous_theta = theta.clone()
+            previous_loss = loss.clone()
+        
+        loss = optim.step(closure)
+        
+        if iteration > 0:
+            d_loss = previous_loss - loss
+            d_theta = torch.norm(previous_theta - theta, p=2)
+            grad_norm = torch.norm(optim.param_groups[0]["params"][0].grad, p=2)
+            pbar.set_postfix({"grad_norm": grad_norm, "d_theta": d_theta, "d_loss": d_loss})
+            if d_loss < 1e-5 and d_theta < 1e-5 and grad_norm < 1e-5:
+                break
+            
+    torch.save(theta, f"{save_dir}/theta.pt")
+    print("theta spearman with CTT: ", spearman_corrcoef(theta, matrix.mean(1)))
--- gather_helm_data/get_zero_column.py
+++ gather_helm_data/get_zero_column.py
@@ -1,21 +1,21 @@
+import os
 import re
 import argparse
 import pandas as pd
 from huggingface_hub import HfApi, snapshot_download
 import pickle
-from vllm import LLM
 from sentence_transformers import SentenceTransformer
 
 scenario2pattern = {
-    "mmlu": [r"(Question:)", "Answer:"],
-    "classic/babi_qa": [r"(Passage:)", "Answer:"],
-    "classic/bbq": [r"(Passage:)", "Answer:"],
+    "mmlu": ["Question:", "Answer:"],
+    "classic/babi_qa": ["Passage:", "Answer:"],
+    "classic/bbq": ["Passage:", "Answer:"],
     "classic/blimp": None, # No fewshot examples
     "classic/bold": None, # No fewshot examples
-    "classic/boolq": [r"(Passage:)", "Answer:"],
-    "classic/civil_comment": [r"(Passage:)", "Answer:"],
+    "classic/boolq": ["Passage:", "Answer:"],
+    "classic/civil_comment": ["Passage:", "Answer:"],
     "classic/code": ["QUESTION:", "\n\n\nUse Standard Input format\n\nANSWER in Python code:\n"],
-    "classic/commonsense": [r"(Question:)", "Answer:"],
+    "classic/commonsense": ["Question:", "Answer:"],
     "classic/copyright": None, # No fewshot examples
     "classic/disinfo": None, # Complex fewshot examples
     "classic/dyck_languague_np=3": ["Input:", None],
@@ -45,8 +45,37 @@ scenario2pattern = {
     "classic/wikifact": ["\n\n", None],
 }
 
-
-
+def extract_last_question(pattern, text):
+    if pattern is None:
+        return text
+    else:
+        start_tag = pattern[0]
+        end_tag = pattern[1]
+        
+        if end_tag is None:
+            match = re.findall(f"({start_tag}.*)", text, re.DOTALL)
+            if match:
+                return match[-1].strip()
+            else:
+                return None
+        
+        elif isinstance(start_tag, list):
+            start_tag1 = pattern[0]
+            start_tag2 = pattern[1]
+            match1 = re.findall(f"({start_tag1}.*?{end_tag})", text, re.DOTALL)
+            match2 = re.findall(f"({start_tag2}.*?{end_tag})", text, re.DOTALL)
+            if match1:
+                return match1[-1].strip()
+            elif match2:
+                return match2[-1].strip()
+            return None
+        
+        else:
+            match = re.findall(f"({start_tag}.*?{end_tag})", text, re.DOTALL)
+            if match:
+                return match[-1].strip()
+            else:
+                return None
 
 context = {
     "mmlu": """
@@ -59,155 +88,126 @@ context = {
         """
 }
 
-dataset_scenario_scenarioid = pd.read_csv("dataset_scenario_scenarioid.csv")
-
-def extract_last_question(pattern, text):
-    # Split into individual questions using 'Question:' as the delimiter
-    questions = re.split(pattern, text)
-  
-    # Remove empty strings and strip whitespace
-    questions = [q.strip() for q in questions if q.strip()]
-    
-    # Get the last question (ignoring the last empty answer)
-    last_question = questions[-1]
-    
-    # Remove the 'Answer:' part if it exists
-    last_question = re.split(r'Answer:', last_question)[0].strip()
-    
-    return last_question
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, required=True)
     args = parser.parse_args()
-
-    repo_id = "stair-lab/reeval_csv"
-    data_folder = snapshot_download(repo_id=repo_id, repo_type="dataset")
-    data = pickle.load(open(f"{data_folder}/{args.dataset}/responses.pkl", "rb"))
-
-    if args.dataset == "mmlu":
-        scenario = "mmlu"
-    elif args.dataset == "classic":
-        scenario = "civil_comment"
-
-    scenarios_id = dataset_scenario_scenarioid[dataset_scenario_scenarioid["scenario"] == scenario]["scenarioid"].values[0]
-    data = data[data["scenarios_id"] == scenarios_id]
-
-
-    question_keys = pd.read_csv(f"{data_folder}/{args.dataset}/instances.csv")
-
-    for i in question_keys.scenarios_id.unique():
-        question_keys1 = question_keys[question_keys["scenarios_id"] == i]
-        print("scenarios_id: ", i)
-        print(len(question_keys1.raw_question.unique()))
-
-    breakpoint()
-    question_keys1 = question_keys[question_keys["scenarios_id"] == scenarios_id]
-    # question_keys_one_pertubation = question_keys[question_keys["perturbation"] == "mild_mix"]
-    # len(question_keys1.raw_question.unique())
-    # >>> 8178 -- this makes sense for 9 demographics groups and about 1000 instances per group
-    # and some groups share questions
-
+    
+    data_folder = snapshot_download(repo_id="stair-lab/reeval_csv", repo_type="dataset")
+    benchmark = args.dataset.split("/")[0]
+    scenario = args.dataset.split("/")[-1]
+    response = pickle.load(open(f"{data_folder}/{benchmark}/responses.pkl", "rb"))
+    sid2scenario = pd.read_csv(f"{data_folder}/{benchmark}/scenarios.csv")
+    question_key = pd.read_csv(f"{data_folder}/{benchmark}/instances.csv")
     model_key = pd.read_csv(f"{data_folder}/model_df.csv")
     flop = pd.read_csv(f"{data_folder}/FLOP.csv")
     flop["model_names_reeval"] = flop["model_names_reeval"].str.replace("_", "/")
-
     # merge model_key with flop: `model_names_reeval` should match `name` in model_key
     # not every name in model_key is in flop, and not every name in flop is in model_key
     # the resulting dataframe should has the same length as model_key
-    model_key_ = pd.merge(model_key, flop, left_on="name", right_on="model_names_reeval", how="left")
+    model_key = pd.merge(model_key, flop, left_on="name", right_on="model_names_reeval", how="left")
+    
+    sid = sid2scenario[sid2scenario['name'] == scenario]['scenarios_id'].iloc[0]
+    response = response[response["scenarios_id"] == sid]
+    question_key = question_key[question_key["scenarios_id"] == sid]
 
     # remove the few-shot example from prompt
-    pattern = scenario2pattern[f"{args.dataset}/{scenario}"]
-    n_questions = len(question_keys["prompt"])
-    last_questions = [extract_last_question(pattern, question_keys["prompt"][i]) for i in range(n_questions)]
-    question_keys["question_content"] = last_questions
+    pattern = scenario2pattern[args.dataset]
+    n_questions = len(question_key["prompt"])
+    print(f"dataset: {args.dataset}, number of question: {n_questions}")
+    extract_example = extract_last_question(pattern, question_key["prompt"][0])
+    print(f"prompt: {question_key['prompt'][0]}\nquestion_content: {extract_example}")
+    breakpoint()
+    last_questions = [extract_last_question(pattern, question_key["prompt"][i]) for i in range(n_questions)]
+    question_key["question_content"] = last_questions
 
-    question_contexts = []
-    for i in range(n_questions):
-        if args.dataset == "mmlu":
-            subject_i = question_keys["subject"][i]
-            subject_i = subject_i.replace("_", " ")
-            mmlu_context = context["mmlu"]
-            question_contexts.append(mmlu_context % subject_i)
-        elif args.dataset == "classic":
-            if scenario == "civil_comment":
-                breakpoint()
-                demographics_group_i = question_keys["demographics"][i]
-                demographics_group_i = demographics_group_i.replace("_", " ")
-                civil_comment_context = context["classic/civil_comment"]
-                question_contexts.append(civil_comment_context % demographics_group_i)
+    # question_contexts = []
+    # for i in range(n_questions):
+    #     if args.dataset == "mmlu":
+    #         subject_i = question_key["subject"][i]
+    #         subject_i = subject_i.replace("_", " ")
+    #         mmlu_context = context["mmlu"]
+    #         question_contexts.append(mmlu_context % subject_i)
+    #     elif args.dataset == "classic":
+    #         if scenario == "civil_comment":
+    #             breakpoint()
+    #             demographics_group_i = question_key["demographics"][i]
+    #             demographics_group_i = demographics_group_i.replace("_", " ")
+    #             civil_comment_context = context["classic/civil_comment"]
+    #             question_contexts.append(civil_comment_context % demographics_group_i)
 
 
-    question_keys["question_contexts"] = question_contexts
+    # question_key["question_contexts"] = question_contexts
 
     # merge data and instances by instance_id
     # instance_id in data is float, while in instances is int
     # but they are the same
-    data["instance_id"] = data["instance_id"].astype(int)
-    data = pd.merge(data, question_keys, on="instance_id")
+    response["instance_id"] = response["instance_id"].astype(int)
+    data = pd.merge(response, question_key, on="instance_id")
 
     # merge data with model_key by model_id
     data["model_id"] = data["model_id"].astype(int)
     data = pd.merge(data, model_key, on="model_id")   
 
     # save the data to parquet form
-    data.to_parquet("data.parquet")
+    save_dir = f"../data/another_repo/{scenario}"
+    os.makedirs(save_dir, exist_ok=True)
+    data.to_parquet(f"{save_dir}/data.parquet")
 
-    # upload the data to huggingface hub
-    api = HfApi()
-    # api.create_repo("stair-lab/reeval_another_repo", repo_type="dataset", exist_ok=True)
-    api.upload_file(
-        repo_id="stair-lab/reeval_another_repo",
-        path_in_repo="mmlu/data.parquet",
-        path_or_fileobj="data.parquet",
-        repo_type="dataset"
-    )
+    # # upload the data to huggingface hub
+    # api = HfApi()
+    # # api.create_repo("stair-lab/reeval_another_repo", repo_type="dataset", exist_ok=True)
+    # api.upload_file(
+    #     repo_id="stair-lab/reeval_another_repo",
+    #     path_in_repo=f"{scenario}/data.parquet",
+    #     path_or_fileobj="data.parquet",
+    #     repo_type="dataset"
+    # )
 
-    # add a column for embedding (each embedding is a list of 4096 floats)
-    model_name = "Alibaba-NLP/gte-Qwen2-7B-instruct" # "intfloat/e5-mistral-7b-instruct"
-    # model = LLM(model=model_name, enforce_eager=True)
-    model = SentenceTransformer(model_name, trust_remote_code=True)
+    # # add a column for embedding (each embedding is a list of 4096 floats)
+    # model_name = "Alibaba-NLP/gte-Qwen2-7B-instruct" # "intfloat/e5-mistral-7b-instruct"
+    # # model = LLM(model=model_name, enforce_eager=True)
+    # model = SentenceTransformer(model_name, trust_remote_code=True)
 
-    pool = model.start_multi_process_pool()
+    # pool = model.start_multi_process_pool()
 
-    embed_context_and_question_together = True
-    if embed_context_and_question_together:
-        content_to_embed = [context + question for context, question in zip(question_contexts, last_questions)]
-        embeddings = model.encode_multi_process(content_to_embed, pool, show_progress_bar=True, batch_size=32)
-        model.stop_multi_process_pool(pool)
+    # embed_context_and_question_together = True
+    # if embed_context_and_question_together:
+    #     content_to_embed = [context + question for context, question in zip(question_contexts, last_questions)]
+    #     embeddings = model.encode_multi_process(content_to_embed, pool, show_progress_bar=True, batch_size=32)
+    #     model.stop_multi_process_pool(pool)
 
-        embeddings = [emb.outputs.embedding for emb in embeddings]
-        question_keys[f"embeddings_{model_name.replace('/', '_')}"] = embeddings_last_questions
-        question_keys.to_parquet("question.parquet")
+    #     embeddings = [emb.outputs.embedding for emb in embeddings]
+    #     question_key[f"embeddings_{model_name.replace('/', '_')}"] = embeddings_last_questions
+    #     question_key.to_parquet("question.parquet")
 
-        api.upload_file(
-            repo_id="stair-lab/reeval_another_repo",
-            path_in_repo="mmlu/question.parquet",
-            path_or_fileobj="question.parquet",
-            repo_type="dataset"
-        )
+    #     api.upload_file(
+    #         repo_id="stair-lab/reeval_another_repo",
+    #         path_in_repo="mmlu/question.parquet",
+    #         path_or_fileobj="question.parquet",
+    #         repo_type="dataset"
+    #     )
 
-    else:
-        embeddings_question_contexts = model.encode_multi_process(question_contexts, pool, show_progress_bar=True, batch_size=32)
-        embeddings_last_questions = model.encode_multi_process(last_questions, pool, show_progress_bar=True, batch_size=32)
-        model.stop_multi_process_pool(pool)
+    # else:
+    #     embeddings_question_contexts = model.encode_multi_process(question_contexts, pool, show_progress_bar=True, batch_size=32)
+    #     embeddings_last_questions = model.encode_multi_process(last_questions, pool, show_progress_bar=True, batch_size=32)
+    #     model.stop_multi_process_pool(pool)
 
-        embeddings_question_contexts = [emb.tolist() for emb in embeddings_question_contexts]
-        embeddings_last_questions = [emb.tolist() for emb in embeddings_last_questions]
-        question_keys[f"embeddings_question_contexts_{model_name.replace('/', '_')}"] = embeddings_question_contexts
-        question_keys[f"embeddings_last_questions_{model_name.replace('/', '_')}"] = embeddings_last_questions
-        question_keys.to_parquet("question_separated_emb.parquet")
+    #     embeddings_question_contexts = [emb.tolist() for emb in embeddings_question_contexts]
+    #     embeddings_last_questions = [emb.tolist() for emb in embeddings_last_questions]
+    #     question_key[f"embeddings_question_contexts_{model_name.replace('/', '_')}"] = embeddings_question_contexts
+    #     question_key[f"embeddings_last_questions_{model_name.replace('/', '_')}"] = embeddings_last_questions
+    #     question_key.to_parquet("question_separated_emb.parquet")
 
-        api.upload_file(
-            repo_id="stair-lab/reeval_another_repo",
-            path_in_repo="mmlu/question_separated_emb.parquet",
-            path_or_fileobj="question_separated_emb.parquet",
-            repo_type="dataset"
-        )
+    #     api.upload_file(
+    #         repo_id="stair-lab/reeval_another_repo",
+    #         path_in_repo="mmlu/question_separated_emb.parquet",
+    #         path_or_fileobj="question_separated_emb.parquet",
+    #         repo_type="dataset"
+    #     )
 
-    # load the data from huggingface
-    data_folder = snapshot_download(repo_id="stair-lab/reeval_another_repo", repo_type="dataset")
-    data = pd.read_parquet(f"{data_folder}/mmlu/data.parquet", engine="fastparquet")
-    question_keys = pd.read_parquet(f"{data_folder}/mmlu/question.parquet", engine="fastparquet")
-    question_keys = pd.read_parquet(f"{data_folder}/mmlu/question_separated_emb.parquet", engine="fastparquet")
+    # # load the data from huggingface
+    # data_folder = snapshot_download(repo_id="stair-lab/reeval_another_repo", repo_type="dataset")
+    # data = pd.read_parquet(f"{data_folder}/mmlu/data.parquet", engine="fastparquet")
+    # question_key = pd.read_parquet(f"{data_folder}/mmlu/question.parquet", engine="fastparquet")
+    # question_key = pd.read_parquet(f"{data_folder}/mmlu/question_separated_emb.parquet", engine="fastparquet")
